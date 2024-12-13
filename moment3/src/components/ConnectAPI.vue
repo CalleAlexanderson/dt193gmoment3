def3338bf9b58d911362b22472d76569b0cd2c00
CalleAlexanderson <calle.alexanderson@telia.com>
2024-12-13 01:49:12 +0100

    lade till axios för att hämta data från api

---
+++ moment3/src/components/ConnectAPI.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>API connection</p>
+</template>
+
+<script>
+    import axios from 'axios';
+    console.log(axios.isCancel('something'));
+    axios.get('http://127.0.0.1:3000/books')
+  .then(function (response) {
+    // handle success
+    console.log("funkar");
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+</script>
